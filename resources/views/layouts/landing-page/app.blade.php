c6b01862517eb9f72fb32190c0e00f14e6149689
Fahris12344 <farismhd255@gmail.com>
2024-11-20 21:35:28 +0700

    pemisahan landing

--- resources/views/layouts/landing-page/app.blade.php
+++ resources/views/layouts/landing-page/app.blade.php
@@ -2,76 +2,73 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <title>Stocker - Stock Market Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-  <title>@yield('title', 'SMK AL AHZAR')</title> <!-- Bagian untuk judul halaman -->
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 
-  <!-- Favicons -->
-  <link href="assets/img/logo.jpeg" rel="icon">
-  <link href="assets/img/logo.jpeg" rel="apple-touch-icon">
 
-  <!-- Google Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:wght@400;500;700;900&display=swap"
+        rel="stylesheet">
 
-  <!-- Google Icons -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <!-- Libraries Stylesheet -->
+    <link rel="stylesheet" href="assets/lib/animate/animate.min.css" />
+    <link href="assets/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
 
-  <!-- Tambahkan CSS Khusus Halaman -->
-  @stack('styles')
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
-  <!-- Header -->
-  <topbar>
-    @include('layouts.landing-page.topbar') <!-- Include header -->
-  </topbar>
 
-  <!-- Konten Utama -->
-  <main>
-    @yield('content') <!-- Bagian konten dinamis -->
-  </main>
+    <!-- Header -->
+    <topbar>
+        @include('layouts.landing-page.topbar') <!-- Include header -->
+    </topbar>
 
-  <!-- Footer -->
-  <footer>
-    @include('layouts.landing-page.footer') <!-- Include footer -->
-  </footer>
+    <!-- Konten Utama -->
+    <main>
+        @yield('content') <!-- Bagian konten dinamis -->
+    </main>
 
-  <!-- jQuery -->
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-  <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <!-- Footer -->
+    <footer>
+        @include('layouts.landing-page.footer') <!-- Include footer -->
+    </footer>
 
-  <!-- Template Main JS File -->
+  <!-- JavaScript Libraries -->
+  <script src="assets/https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="assets/https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/lib/wow/wow.min.js"></script>
+  <script src="assets/lib/easing/easing.min.js"></script>
+  <script src="assets/lib/waypoints/waypoints.min.js"></script>
+  <script src="assets/lib/counterup/counterup.min.js"></script>
+  <script src="assets/lib/lightbox/js/lightbox.min.js"></script>
+  <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  
+  <!-- Template Javascript -->
   <script src="assets/js/main.js"></script>
-  <script src="assets/js/fitur.js"></script>
-  <script src="forms/contact.js"></script>
-  <!-- <script src="assets/js/user_device.js"></script>
-  <script src="assets/js/display_device.js"></script> -->
 </body>
 
 </html>
--- resources/views/layouts/landing-page/footer.blade.php
+++ resources/views/layouts/landing-page/footer.blade.php
@@ -1,136 +1,104 @@
-  <!-- Footer -->
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
+   <!-- Footer Start -->
+   <div class="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.2s">
+    <div class="container py-5 border-start-0 border-end-0" style="border: 1px solid; border-color: rgb(255, 255, 255, 0.08);">
+        <div class="row g-5">
+            <div class="col-md-6 col-lg-6 col-xl-4">
+                <div class="footer-item">
+                    <a href="index.html" class="p-0">
+                        <h4 class="text-white"><i class="fas fa-search-dollar me-3"></i>Stocker</h4>
+                        <!-- <img src="assets/img/logo.png" alt="Logo"> -->
+                    </a>
+                    <p class="mb-4">Dolor amet sit justo amet elitr clita ipsum elitr est.Lorem ipsum dolor sit amet, consectetur adipiscing...</p>
+                    <div class="d-flex">
+                        <a href="#" class="bg-primary d-flex rounded align-items-center py-2 px-3 me-2">
+                            <i class="fas fa-apple-alt text-white"></i>
+                            <div class="ms-3">
+                                <small class="text-white">Download on the</small>
+                                <h6 class="text-white">App Store</h6>
+                            </div>
+                        </a>
+                        <a href="#" class="bg-dark d-flex rounded align-items-center py-2 px-3 ms-2">
+                            <i class="fas fa-play text-primary"></i>
+                            <div class="ms-3">
+                                <small class="text-white">Get it on</small>
+                                <h6 class="text-white">Google Play</h6>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-2">
+                <div class="footer-item">
+                    <h4 class="text-white mb-4">Quick Links</h4>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> About Us</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Feature</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Attractions</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Tickets</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Blog</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Contact us</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3">
+                <div class="footer-item">
+                    <h4 class="text-white mb-4">Support</h4>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Privacy Policy</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Terms & Conditions</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Disclaimer</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Support</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> FAQ</a>
+                    <a href="#"><i class="fas fa-angle-right me-2"></i> Help</a>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3">
+                <div class="footer-item">
+                    <h4 class="text-white mb-4">Contact Info</h4>
+                    <div class="d-flex align-items-center">
+                        <i class="fas fa-map-marker-alt text-primary me-3"></i>
+                        <p class="text-white mb-0">123 Street New York.USA</p>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <i class="fas fa-envelope text-primary me-3"></i>
+                        <p class="text-white mb-0">info@example.com</p>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <i class="fa fa-phone-alt text-primary me-3"></i>
+                        <p class="text-white mb-0">(+012) 3456 7890</p>
+                    </div>
+                    <div class="d-flex align-items-center mb-4">
+                        <i class="fab fa-firefox-browser text-primary me-3"></i>
+                        <p class="text-white mb-0">Yoursite@ex.com</p>
+                    </div>
+                    <div class="d-flex">
+                        <a class="btn btn-primary btn-sm-square rounded-circle me-3" href="#"><i class="fab fa-facebook-f text-white"></i></a>
+                        <a class="btn btn-primary btn-sm-square rounded-circle me-3" href="#"><i class="fab fa-twitter text-white"></i></a>
+                        <a class="btn btn-primary btn-sm-square rounded-circle me-3" href="#"><i class="fab fa-instagram text-white"></i></a>
+                        <a class="btn btn-primary btn-sm-square rounded-circle me-0" href="#"><i class="fab fa-linkedin-in text-white"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
 
-      <div class="footer-newsletter">
-          <div class="container">
-              <div class="row justify-content-center">
-                  <div class="col-lg-6">
-                      <!-- <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="#" method="post" class="text-center">
-              <a href="https://www.youtube.com/@smkalazharsempu" class="btn btn-primary rounded-2">Subscribe</a>
-            </form> -->
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="footer-top">
-          <div class="container">
-              <div class="row">
-
-                  <div class="col-lg-3 col-md-6 footer-contact">
-                      <h3>SMK AL AHZAR</h3>
-                      <p>
-                          Jl. Imam Syafi'i No.45,<br>
-                          Tugung, Sempu, Kec. Sempu,<br>
-                          Kabupaten Banyuwangi, Jawa Timur 68468 <br><br>
-                          <strong>Phone:</strong> +62 852 3655 3650<br>
-                          <strong>Email:</strong> smkalazhar.sempubwi@gmail.com<br>
-                      </p>
-                  </div>
-
-                  <div class="col-lg-3 col-md-6 footer-links">
-                      <h4>Useful Links</h4>
-                      <ul>
-                          <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                          <li><i class="bx bx-chevron-right"></i> <a href="#about">Visi | Misi</a></li>
-                          <li><i class="bx bx-chevron-right"></i> <a href="jurusan.html">jurusan</a></li>
-                          <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li> -->
-                      </ul>
-                  </div>
-
-                  <div class="col-lg-3 col-md-6 footer-links">
-                      <h4>Our Services</h4>
-                      <ul>
-                          <li><i class="bx bx-chevron-right"></i><a href="https://class.hummatech.com/login"
-                                  target="_blank">Hummantech</a></li>
-                          <li><i class="bx bx-chevron-right"></i> <a href="https://acsmultitechnology.com/"
-                                  target="_blank">ACS -
-                                  Multitechnology</a></li>
-                          <li><i class="bx bx-chevron-right"></i> <a href="404/txt.html">Update Yang akan Datang?</a>
-                          </li>
-                      </ul>
-                  </div>
+<!-- Copyright Start -->
+<div class="container-fluid copyright py-4">
+    <div class="container">
+        <div class="row g-4 align-items-center">
+            <div class="col-md-6 text-center text-md-start mb-md-0">
+                <span class="text-body"><a href="#" class="border-bottom text-white"><i class="fas fa-copyright text-light me-2"></i>Your Site Name</a>, All right reserved.</span>
+            </div>
+            <div class="col-md-6 text-center text-md-end text-body">
+                <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
+                <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
+                <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
+                Designed By <a class="border-bottom text-white" href="https://htmlcodex.com">HTML Codex</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Copyright End -->
 
 
-                  <!-- <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li> -->
-                  <div class="col-lg-3 col-md-6 footer-links">
-                      <h4>Ikuti Kami di Medai Lainya</h4>
-                      <p>Kalian bisa Follow Media sosial kami tidak ketinggalan Informasi</p>
-                      <p id="setime"></p>
-                      <div class="social-links mt-3">
-                          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                          <a href="https://www.youtube.com/@smkalazharsempu" class="google-plus" target="_blank"><i
-                                  class="bx bxl-youtube"></i></i></a>
-                          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-                      </div>
-                  </div>
-
-              </div>
-          </div>
-      </div>
-      <!-- End Footer -->
-      <div class="container footer-bottom clearfix">
-          <div class="copyright">
-              &copy; Copyright <strong><span>Zaki</span></strong>. All Rights Reserved
-          </div>
-          <div class="credits">
-              Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-          </div>
-      </div>
-  </footer><!-- End Footer -->
-
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
-      <i class="bi bi-arrow-up-short"></i></a>
-  <!-- settime -->
-  <style>
-      .warna {
-          position: relative;
-          color: aqua;
-          left: 50px;
-      }
-  </style>
-  <script>
-      var date = new Date();
-      var hours = date.getHours();
-
-      var greetings = [
-          "Selamat Malam!",
-          "Selamat Pagi!",
-          "Selamat Siang!",
-          "Selamat Sore!",
-          "Selamat Malam Semuanya!"
-      ];
-
-      var timePeriod = Math.floor(hours / 6); // Divide 24-hour day into 4 segments
-      var greeting = greetings[timePeriod]; // Assign greeting based on time period
-
-      var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober',
-          'November', 'Desember'
-      ];
-      var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
-
-      var day = date.getDate();
-      var month = date.getMonth();
-      var thisDay = date.getDay();
-      thisDay = myDays[thisDay];
-
-      var year = date.getFullYear();
-
-      var info = greeting + "<br>" + thisDay + ", " + day + ' ' + months[month] + ' ' + year;
-      document.getElementById('setime').innerHTML = info;
-
-      // Tambahkan event listener untuk menampilkan alert saat cursor diarahkan ke elemen tersebut
-      document.getElementById('setime').addEventListener('', function() {
-          var alertInfo = info.replace("<br>", ", ");
-          alert(alertInfo);
-      });
-  </script>
-  <!-- end setime -->
+<!-- Back to Top -->
+<a href="#" class="btn btn-primary btn-lg-square rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>   
