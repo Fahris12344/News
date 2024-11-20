88cf4d11efa2b357f9748878b882b68d604e08e0
ManukMabor <149447571+Alung85@users.noreply.github.com>
2024-11-21 01:51:29 +0700

    fix

--- resources/views/layouts/Admin-page/sidebar.blade.php
+++ resources/views/layouts/Admin-page/sidebar.blade.php
@@ -8,32 +8,6 @@
             </a>            
           </li>
           <li class="nav-item">
-<<<<<<< Updated upstream
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-              <i class="icon-layout menu-icon"></i>
-              <span class="menu-title">UI Elements</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-              <i class="icon-columns menu-icon"></i>
-              <span class="menu-title">Form elements</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="form-elements">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"><a class="nav-link" href="pages/forms/basic_elements.html">Basic Elements</a></li>
-              </ul>
-            </div>
-=======
             <a class="nav-link" data-toggle="collapse" href="/admin/news" aria-expanded="false" aria-controls="ui-basic">
               <i class="bi bi-newspaper" style="font-size:18px;"></i>
               <span class="menu-title" style="margin-left:18px;">Berita</span>
@@ -44,7 +18,6 @@
             <i class="bi bi-tags" style="font-size:18px;"></i>
               <span class="menu-title" style="margin-left:18px;">Kategori</span>
             </a>
->>>>>>> Stashed changes
           </li>
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="/admin/announcement" aria-expanded="false" aria-controls="charts">
@@ -53,43 +26,6 @@
             </a>
           </li>
           <li class="nav-item">
-<<<<<<< Updated upstream
-            <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-              <i class="icon-grid-2 menu-icon"></i>
-              <span class="menu-title">Tables</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="tables">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/tables/basic-table.html">Basic table</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#icons" aria-expanded="false" aria-controls="icons">
-              <i class="icon-contract menu-icon"></i>
-              <span class="menu-title">Icons</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="icons">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/icons/mdi.html">Mdi icons</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <i class="icon-head menu-icon"></i>
-              <span class="menu-title">User Pages</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-              </ul>
-            </div>
-=======
             <a class="nav-link" data-toggle="collapse" href="/admin/user" aria-expanded="false" aria-controls="auth">
             <i class="bi bi-person-badge" style="font-size:18px;"></i>
               <span class="menu-title" style="margin-left:18px;">User</span>
@@ -101,7 +37,6 @@
             <i class="bi bi-chat-left-dots" style="font-size:18px;"></i>
               <span class="menu-title" style="margin-left:18px;">Komentar</span>
             </a>
->>>>>>> Stashed changes
           </li>
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#error" aria-expanded="false" aria-controls="error">
