0310ae4825f25bdeca981fe768cc27875db0110b
FrontierMar <rozakadm@gmail.com>
2024-11-21 01:13:54 +0700

    Update dashboard.blade.php

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -78,27 +78,27 @@
 
             <!-- Statistics Section -->
             <div class="row">
-                <div class="col-md-6 grid-margin stretch-card">
-                    <div class="card tale-bg">
-                        <div class="card-people mt-auto">
-                            <img src="images/dashboard/people.svg" alt="people">
-                            <div class="weather-info">
-                                <div class="d-flex">
-                                  <div>
+              <div class="col-md-6 grid-margin stretch-card">
+                <div class="card tale-bg">
+                    <div class="card-people mt-auto">
+                        <img src="images/dashboard/people.svg" alt="people">
+                        <div class="weather-info">
+                            <div class="d-flex">
+                                <div>
                                     <!-- Logo cuaca, seperti ikon matahari -->
-                                    <h2 class="mb-7 font-weight-normal">
+                                    <h2 class="mb-7 font-weight-normal" style="margin-top: -30px">
                                         <i class="icon-sun mr-2"></i>31<sup>C</sup>
                                     </h2>
                                 </div>
-                                <div class="ml-2">
+                                <div class="ml-2" style="margin-top: -30px;"> <!-- Menambahkan margin-top untuk menggeser ke atas -->
                                     <h4 class="location font-weight-normal">Bangalore</h4>
                                     <h6 class="font-weight-normal">India</h6>
-                                </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>            
                 <div class="col-md-6 grid-margin transparent">
                     <div class="row">
                         <div class="col-md-6 mb-4 stretch-card transparent">
