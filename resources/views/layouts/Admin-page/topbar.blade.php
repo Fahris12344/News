4f7655174765f7658eb80f76bb32a3456e8ef30e
farl <shinjoakane078@gmail.com>
2024-11-21 01:52:30 +0700

    cumicumi

--- resources/views/layouts/Admin-page/topbar.blade.php
+++ resources/views/layouts/Admin-page/topbar.blade.php
@@ -67,21 +67,6 @@
             </a>
           </div>
         </li>
-        <li class="nav-item nav-profile dropdown">
-          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-            <img src="{{ asset('assets-admin/images/faces/face28.jpg') }}" alt="profile" />
-          </a>
-          <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-            <a class="dropdown-item">
-              <i class="ti-settings text-primary"></i>
-              Settings
-            </a>
-            <a class="dropdown-item">
-              <i class="ti-power-off text-primary"></i>
-              Logout
-            </a>
-          </div>
-        </li>
         <li class="nav-item nav-settings d-none d-lg-flex">
           <a class="nav-link" href="#">
             <i class="icon-ellipsis"></i>
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -51,9 +51,8 @@
                 <div class="col-md-12 grid-margin">
                     <div class="row">
                         <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                            <h3 class="font-weight-bold">Welcome Aamir</h3>
-                            <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span
-                                    class="text-primary">3 unread alerts!</span></h6>
+                            <h3 class="font-weight-bold">Welcome Atmin</h3>
+                            <h6 class="font-weight-normal mb-0" style="line-height:1.5;">Anda dapat mengatur apa yang boleh dan tidak boleh ada di website ini, anda dapat menghapus atau memblokir berita dan akun user.</h6>
                         </div>
                         <div class="col-12 col-xl-4">
                             <div class="justify-content-end d-flex">
@@ -81,21 +80,7 @@
               <div class="col-md-6 grid-margin stretch-card">
                 <div class="card tale-bg">
                     <div class="card-people mt-auto">
-                        <img src="images/dashboard/people.svg" alt="people">
-                        <div class="weather-info">
-                            <div class="d-flex">
-                                <div>
-                                    <!-- Logo cuaca, seperti ikon matahari -->
-                                    <h2 class="mb-7 font-weight-normal" style="margin-top: -30px">
-                                        <i class="icon-sun mr-2"></i>31<sup>C</sup>
-                                    </h2>
-                                </div>
-                                <div class="ml-2" style="margin-top: -30px;"> <!-- Menambahkan margin-top untuk menggeser ke atas -->
-                                    <h4 class="location font-weight-normal">Bangalore</h4>
-                                    <h6 class="font-weight-normal">India</h6>
-                                </div>
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
             </div>            
@@ -104,18 +89,16 @@
                         <div class="col-md-6 mb-4 stretch-card transparent">
                             <div class="card card-tale">
                                 <div class="card-body">
-                                    <p class="mb-4">Today’s Bookings</p>
+                                    <p class="mb-4">Total Pengguna</p>
                                     <p class="fs-30 mb-2">4006</p>
-                                    <p>10.00% (30 days)</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6 mb-4 stretch-card transparent">
                             <div class="card card-dark-blue">
                                 <div class="card-body">
-                                    <p class="mb-4">Total Bookings</p>
+                                    <p class="mb-4">Total Berita</p>
                                     <p class="fs-30 mb-2">61344</p>
-                                    <p>22.00% (30 days)</p>
                                 </div>
                             </div>
                         </div>
@@ -124,18 +107,16 @@
                         <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
                             <div class="card card-light-blue">
                                 <div class="card-body">
-                                    <p class="mb-4">Number of Meetings</p>
+                                    <p class="mb-4">Berita belum ditanggapi</p>
                                     <p class="fs-30 mb-2">34040</p>
-                                    <p>2.00% (30 days)</p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6 stretch-card transparent">
                             <div class="card card-light-danger">
                                 <div class="card-body">
-                                    <p class="mb-4">Number of Clients</p>
+                                    <p class="mb-4">Pengumuman</p>
                                     <p class="fs-30 mb-2">47033</p>
-                                    <p>0.22% (30 days)</p>
                                 </div>
                             </div>
                         </div>
