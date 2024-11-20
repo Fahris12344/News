6017f5b5a5c2e073b2699299779b57a8e69582cd
silent2601 <febysilent1801@gmail.com>
2024-11-20 18:01:51 +0700

    user controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,6 +21,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+Route::prefix('news')->group(function () {
+    Route::get('/', [NewsController::class, 'index'])->name('news.index'); // Menampilkan daftar berita
+    Route::get('/create', [NewsController::class, 'create'])->name('news.create'); // Form tambah berita
+    Route::post('/', [NewsController::class, 'store'])->name('news.store'); // Proses simpan berita
+    Route::get('/{id}/edit', [NewsController::class, 'edit'])->name('news.edit'); // Form edit berita
+    Route::put('/{id}', [NewsController::class, 'update'])->name('news.update'); // Proses update berita
+    Route::delete('/{id}', [NewsController::class, 'destroy'])->name('news.destroy'); // Proses hapus berita
+});
+
 });
 
 require __DIR__.'/auth.php';
