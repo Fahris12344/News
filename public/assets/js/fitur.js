74f54cbdbd4bc7ff7728a78cf9f2778483899eb3
Fahris12344 <farismhd255@gmail.com>
2024-11-20 17:25:10 +0700

    route

---
+++ public/assets/js/fitur.js
@@ -0,0 +1,17 @@
+// Fungsi untuk menampilkan pemberitahuan
+function showAlert() {
+  Swal.fire({
+    title: "Fitur Segera Datang!",
+    text: "Fitur ini sedang dalam pengembangan dan akan segera tersedia.",
+    icon: "info",
+    confirmButtonText: "OK",
+  });
+}
+function showalert1() {
+  Swal.fire({
+    title: "Neni Anggriyani",
+    text: "Neni Anggriyani Belum Menambahkan Media|Mohon di Tunggu",
+    icon: "error",
+    confirmButtonText: "OK",
+  });
+}
